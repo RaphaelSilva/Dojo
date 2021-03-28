50446188bf2062759bba0a774db663539a894b89
Raphael Silva <eng.raphaelsn@gmail.com>
2021-03-28 01:35:17 -0300

    send all files of project vue3-bootstrap5

---
+++ vue3-bootstrap5/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <MenuSample />
+  <main class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </main>
+  <ModalSample />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue";
+import MenuSample from "@/components/MenuSample.vue";
+import ModalSample from "@/components/ModalSample.vue";
+
+@Options({
+  components: {
+    HelloWorld,
+    MenuSample,
+    ModalSample,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
