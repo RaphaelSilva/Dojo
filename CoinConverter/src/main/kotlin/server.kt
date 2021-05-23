cbf45a01e2961acf001a8d2ce3ba996f25a0b55d
engra <eng.raphaelsn@gmail.com>
2021-05-22 22:15:34 -0300

    new server file created

--- CoinConverter/src/main/kotlin/server.kt
+++ CoinConverter/src/main/kotlin/server.kt
@@ -2,15 +2,6 @@ import io.javalin.Javalin
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-
-object StarWarsFilms : Table() {
-    val id: Column<Int> = integer("id").autoIncrement()
-    val sequelId: Column<Int> = integer("sequel_id").uniqueIndex()
-    val name: Column<String> = varchar("name", 50)
-    val director: Column<String> = varchar("director", 50)
-    override val primaryKey = PrimaryKey(id, name = "PK_StarWarsFilms_Id") // PK_StarWarsFilms_Id is optional here
-}
-
 fun main() {
     val app = Javalin.create().start(7000)
     app.get("/") { ctx -> ctx.result("Hello World") }
