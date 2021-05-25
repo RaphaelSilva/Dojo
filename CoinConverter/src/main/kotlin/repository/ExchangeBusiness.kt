398374320b0302b132f89ebe932d36264c21c61e
engra <eng.raphaelsn@gmail.com>
2021-05-25 20:53:32 -0300

    normalize indentation

--- CoinConverter/src/main/kotlin/repository/ExchangeBusiness.kt
+++ CoinConverter/src/main/kotlin/repository/ExchangeBusiness.kt
@@ -4,18 +4,17 @@ import Exchange
 import bean.ExchangeService
 import java.math.BigDecimal
 
-class ExchangeBusiness (service: ExchangeService) {
-    val service = service
+class ExchangeBusiness(private val service: ExchangeService) {
 
-    fun Convert(value: Double, coin_src: String, coin_dest: String): Exchange {
-        val resp = service.getRates("EUR,$coin_src,$coin_dest").execute()
+    fun convert(value: Double, from: String, to: String): Exchange {
+        val resp = service.getRates("EUR,$from,$to").execute()
         val body = resp.body()
-        val convert = (value / body?.rates?.get(coin_src)!!) * body?.rates?.get(coin_dest)!!
+        val convert = (value / body?.rates?.get(from)!!) * body.rates.get(to)!!
         return Exchange(
-            coin_src, coin_dest,
+            from, to,
             BigDecimal.valueOf(value),
             BigDecimal.valueOf(convert),
-            value / convert
+            convert / value
         )
     }
 }
